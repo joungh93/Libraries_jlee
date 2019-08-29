1e32970ea47ebd41738c5954f3473c5f0f33b8ce
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2019-08-29 12:25:55 +0900

    Update truncate_colormap.py
    Fixed a few misleading variables

--- truncate_colormap.py
+++ truncate_colormap.py
@@ -6,12 +6,16 @@ Created on Thu Nov  1 23:00:59 2018
 @author: jlee
 """
 
+
+# Reference : https://stackoverflow.com/questions/18926031/how-to-extract-a-subset-of-a-colormap-as-a-new-colormap-in-matplotlib
+
+
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import numpy as np
 
-def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+def truncate_colormap(cmap, minval=0.0, maxval=1.0, nn=256):
     new_cmap = colors.LinearSegmentedColormap.from_list(
-        'trunc({n},{a:.2f},{b:.2f})'.format(n=cmap.name, a=minval, b=maxval),
-        cmap(np.linspace(minval, maxval, n)))
+        'trunc({0},{1:.2f},{2:.2f})'.format(cmap.name, minval, maxval),
+        cmap(np.linspace(minval, maxval, nn)))
     return new_cmap
